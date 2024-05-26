6c7e87f0a039e9d0181ef56dd600aff5edcd0894
XS986NH <XS986NH@XWB0RGVQ3.ey.net>
2024-05-26 12:06:11 +0530

    Commit2

---
+++ Test Suites/TS_Chkpoint2.ts
@@ -0,0 +1,21 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>TS_Chkpoint2</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>3</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>true</rerunImmediately>
+   <testSuiteGuid>e35fb183-0a09-4471-ad82-ddf0d6cd659b</testSuiteGuid>
+   <testCaseLink>
+      <guid>b2fea706-58e3-429f-be92-64202b7f8538</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/chkpoint2</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+   </testCaseLink>
+</TestSuiteEntity>
